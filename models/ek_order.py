fb930d8bbc646b6b67fb0eafe2e6ea7f13b38586
oumaima <messai.oumaima28@gmail.com>
2024-04-14 13:33:40 +0100

    create invoices

--- models/ek_order.py
+++ models/ek_order.py
@@ -225,9 +225,10 @@ class EkOrder(models.Model):
                     invoice.action_post()
                     _logger.info("Invoice posted successfully for order '%s'", record.name)
                     for move in invoice:
+                        order_id = self.mapped('sale_line_id.order_id')
                         move.message_post_with_view('mail.message_origin_link',
                                                     values={'self': move,
-                                                            'origin': record},
+                                                            'origin': order_id},
                                                     subtype_id=self.env.ref('mail.mt_note').id
                                                     )
                     # Add the invoice to the Many2many field
