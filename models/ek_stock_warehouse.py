7004bb9865ab6dcf04d85db99195bffc2d734b4a
oumaima <messai.oumaima28@gmail.com>
2024-05-08 15:44:21 +0100

    fixing stock

--- models/ek_stock_warehouse.py
+++ models/ek_stock_warehouse.py
@@ -178,12 +178,8 @@ class StockPicking(models.Model):
 
                 else:
                     numeric_value = line.product_id.list_price
-                if self.location_id:
-                    product_stock = self.env['stock.quant'].search([
-                        ('location_id', '=', self.location_id.id),
-                        ('product_id', '=', line.product_id.id)], limit=1)
-                else:
-                    product_stock = self.env['stock.quant'].search([
+
+                product_stock = self.env['stock.quant'].search([
                         ('location_id', '=', self.location_dest_id.id),
                         ('product_id', '=', line.product_id.id)], limit=1)
 
