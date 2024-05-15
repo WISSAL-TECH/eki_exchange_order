1c65ddc8bfc035de6eb54c6b826b83b608f05aea
oumaima <messai.oumaima28@gmail.com>
2024-05-15 16:15:20 +0100

    receive price with order

--- models/ek_order.py
+++ models/ek_order.py
@@ -139,7 +139,7 @@ class EkOrder(models.Model):
                             'name': product.name,
                             'product_uom': product.uom_id.id,
                             'state': 'sale',
-                            'price_unit': line['prix'],
+                            'price_unit': line['price'],
                             'order_id': rec.id
                         }
                         self.env['sale.order.line'].create(order_values)
